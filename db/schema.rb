3236eb50c96e22a0044013ee168ba1bfeb67595e
sethcarrino <sethcarrino@gmail.com>
2022-09-09 16:16:27 -0400

    updated authentication - can now officially sign up and create users properly

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_09_183500) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["reset_password_token"],
+            name: "index_users_on_reset_password_token",
+            unique: true
   end
-
 end
